547f6ef99dc19d2d47ad7bc5c6b3697b22fa13cf
Ardi Loot <ardi.loot@outlook.com>
2021-02-09 22:24:43 +0200

    first commit

---
+++ meter_digits_recognizer/_meter_digits_recognizer.py
@@ -0,0 +1,27 @@
+__all__ = ["MeterDigitsRecognizer"]
+
+import os
+import torch
+import numpy as np
+from meter_digits_recognizer._net import Net
+
+
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+class MeterDigitsRecognizer:
+
+    def __init__(self):
+        self.net = Net()
+        self.net.load_state_dict(torch.load(os.path.join(PACKAGE_DIR, "model_weights.pt"), map_location="cpu"))
+
+    def run(self, digit_imgs):
+        digits_batch = torch.cat([self.net.test_transform(img).unsqueeze(0) for img in digit_imgs], 0)
+        with torch.no_grad():
+            outputs = self.net.forward(digits_batch)
+        all_confidences = torch.nn.functional.softmax(outputs, 1).numpy()
+        predictions = np.argmax(all_confidences, axis=1)
+        confidences = all_confidences[np.arange(len(predictions)), predictions]
+        return predictions, confidences
+
+if __name__ == "__main__":
+    pass
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="meter-digits-recognizer",
+    version="1.0",
+    url="https://github.com/ardiloot/meter-digits-recognizer",
+    author="Ardi Loot",
+    author_email="ardi.loot@outlook.com",
+    packages=["meter_digits_recognizer"],
+    zip_safe=False,
+    include_package_data=True
+)
