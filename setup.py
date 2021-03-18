5fcd2e10a5eda7ed251c1c0ccaf9d6da73e68708
Ardi Loot <ardi.loot@outlook.com>
2021-03-19 00:29:22 +0200

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="meter-digits-recognizer",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/ardiloot/meter-digits-recognizer",
     author="Ardi Loot",
     author_email="ardi.loot@outlook.com",
