09f6ce7268ba384de227e37f8c814ee4619d24c0
Ardi Loot <ardi.loot@outlook.com>
2021-10-30 18:35:20 +0300

    increased model size

--- meter_digits_recognizer/_net.py
+++ meter_digits_recognizer/_net.py
@@ -1,3 +1,4 @@
+import PIL
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as transforms
@@ -10,34 +11,51 @@ class Net(nn.Module):
         transforms.ToTensor()
     ]) 
 
+    if hasattr(transforms, "InterpolationMode"):
+        interp_kwargs = dict(interpolation=transforms.InterpolationMode.BILINEAR)
+    else:
+        interp_kwargs = dict(resample=PIL.Image.BILINEAR)
     train_transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.ColorJitter(brightness=0.1),
         transforms.RandomAffine(5.0,  translate=(0.1, 0.1), scale=(1.0, 1.2),
-            interpolation=transforms.InterpolationMode.BILINEAR),
+            ),
     ])
 
     def __init__(self):
         nn.Module.__init__(self)
-        self.n_filters1 = 32
+        self.n_filters1 = 64
         self.n_filters2 = 64
-        self.n_filters3 = 64
+        self.n_filters3 = 128
+        self.n_filters4 = 128
+        self.n_fc1 = 256
+        self.n_fc2 = 128
+        self.n_fc_input = 5 * 2 * self.n_filters4
         self.input_size = (20, 32)
       
         self.batchnorm = nn.BatchNorm2d(3)
-        self.conv1 = nn.Conv2d(3, self.n_filters1, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(self.n_filters1, self.n_filters2, 3, padding=1)
-        self.conv3 = nn.Conv2d(self.n_filters2, self.n_filters3, 3, padding=1)
-        self.fc1 = nn.Linear(4 * 2 * self.n_filters3, 512)
-        self.fc2 = nn.Linear(512, 11)
+        self.conv1 = nn.Conv2d(3, self.n_filters1, 3, padding=0)
+        self.conv2 = nn.Conv2d(self.n_filters1, self.n_filters2, 3, padding=0)
+        self.conv3 = nn.Conv2d(self.n_filters2, self.n_filters3, 3, padding=0)
+        self.conv4 = nn.Conv2d(self.n_filters3, self.n_filters4, 3, padding=0)
+        self.fc1 = nn.Linear(self.n_fc_input, self.n_fc1)
+        self.fc2 = nn.Linear(self.n_fc1, self.n_fc2)
+        self.fc3 = nn.Linear(self.n_fc2, 11)
 
     def forward(self, x):
         x = self.batchnorm(x)
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = x.view(-1, 4 * 2 * self.n_filters3)
+        
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.pool(x)
+        
+        x = x.view(-1, self.n_fc_input)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
